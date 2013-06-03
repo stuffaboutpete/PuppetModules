db0ef3c70fd31af2654c3971925c0bb483435aef
Pete Smith <pete@peteonline.co.uk>
2013-06-03 10:11:01 +0000

    Changes to way php class runs and added support for enabling errors
    Changes are an attempt to make PHP 5.4 install properly. This is
    still not working but is much closer to working.

--- php/manifests/init.pp
+++ php/manifests/init.pp
@@ -1,17 +1,51 @@
-class php ($version = '5.4') {
+class php ($version = '5.4', $show_errors = false) {
 	
 	include apache2
-	
-	if $version == '5.4' {
-		
-		apt::repository { 'ppa:ondrej/php5': }
-		
-	}
 	
 	package { 'php5':
 		ensure  => present,
 		notify  => Service['apache2'],
 		require => Class['apt::update']
+	}
+	
+	package { 'libapache2-mod-php5':
+		ensure  => present,
+		require => Package['php5']
+	}
+
+	if $show_errors {
+		$ini_file = 'php-development'
+	} else {
+		$ini_file = 'php-production'
+	}
+	
+	file { 'php.ini':
+		path    => '/etc/php5/apache2/php.ini',
+		ensure  => present,
+		source  => "puppet:///modules/php/${ini_file}.ini",
+		notify  => Service['apache2'],
+		require => Package['libapache2-mod-php5']
+	}
+	
+	if $version == '5.4' {
+		
+		apt::repository { 'ppa:ondrej/php5':
+			require => Package['php5']
+		}
+		
+		exec { 'reupdate apt':
+			command => 'apt-get update',
+			path    => '/usr/bin',
+			require => Apt::Repository['ppa:ondrej/php5']
+		}
+		
+		exec { 'reinstall php5.4':
+			command => 'apt-get install php5 -y --force-yes',
+			path    => '/usr/bin',
+			require => Exec['reupdate apt'],
+			before  => File['php.ini']
+		}
+		
 	}
 	
 }

622966574b7328a7a338127db27bd5265f0810d1
Pete Smith <pete@peteonline.co.uk>
2013-06-09 17:58:28 +0000

    Added "pretty log" format to git - not yet working though

--- git/manifests/init.pp
+++ git/manifests/init.pp
@@ -4,4 +4,10 @@ class git {
 		ensure => present
 	}
 	
+	exec { 'pretty-logger':
+		command => 'git config --global alias.prettylog "log --color --graph --pretty=format:\'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset\' --abbrev-commit"',
+		path    => '/usr/bin',
+		require => Package['git']
+	}
+	
 }

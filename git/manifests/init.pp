6c14b39dca8940a0765d4275e7fe0be5519432a9
Pete Smith <pete@peteonline.co.uk>
2014-09-16 14:34:05 +0100

    Removes pretty logger from git module

--- git/manifests/init.pp
+++ git/manifests/init.pp
@@ -7,10 +7,4 @@ class git {
 		require => Class['apt::update']
 	}
 	
-	exec { 'pretty-logger':
-		command => 'git config --global alias.prettylog "log --color --graph --pretty=format:\'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset\' --abbrev-commit"',
-		path    => '/usr/bin',
-		require => Package['git']
-	}
-	
 }
